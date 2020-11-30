11564ec254bca4f7335611b3ec1f56df7599a366
Jonas Gehrlein <jonas@web3.foundation>
2020-11-30 16:06:13 +0100

    implemented writeCSV function

--- src/fileWorker.js
+++ src/fileWorker.js
@@ -14,3 +14,39 @@ export function readJSON(filePath) {
 
     return JSON.parse(rawContent);
   }
+
+  export function writeCSV(obj, name){
+      const filename = name;
+
+      fs.writeFileSync(filename, extractAsCSV(obj), err => {
+          if(err) {
+              console.log('Error writing .csv file', err);
+          } else {
+              console.log('Exported as .csv file');
+          }  
+      });
+  }
+
+  function extractAsCSV(obj){
+    const header_list = [
+        "Day, Price in " + obj.currency +
+        ", Staking Rewards in" + ((obj.network == 'polkadot') ? ' DOT' : ' KSM') + 
+        ", Number of Payouts" +
+        ", Value in Fiat" +
+        ", Income tax (" + (obj.incomeTax*100).toFixed(2) + "%)"
+    ];
+    
+    // remove days without a staking reward.
+    for(let i = 0; i < obj.data.list.length; i++){
+          if(obj.data.list[i].numberPayouts==0){
+              obj.data.list.splice(i,1);
+        }
+    }    
+
+    const rows_list = obj.data.list.map(entry => {
+        if(entry.numberPayouts>0){
+          return `${entry.day}, ${entry.price}, ${entry.amountHumanReadable}, ${entry.numberPayouts}, ${entry.valueFiat}, ${entry.valueTaxable}`;
+        } 
+    });
+      return header_list.concat(rows_list).join("\n");
+  }
