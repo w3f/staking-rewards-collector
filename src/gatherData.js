3a0d688ec077d2c5a4a3ff2ff244964fb55130eb
Jonas Gehrlein <jonas@web3.foundation>
2021-02-05 20:36:58 +0100

    refactor for multi-address format

--- src/gatherData.js
+++ src/gatherData.js
@@ -2,16 +2,8 @@ import { addPriceData } from "./api.js";
 import { makeDaysArray, initializeObject } from "./utils.js";
 import { addStakingData } from './curl.js';
 
-export async function gatherData(userInput){
-    let start = userInput.start;
-    let end = userInput.end;
-    let network = userInput.network;
-    let address = userInput.address;
-    let currency = userInput.currency;
-    let incomeTax = userInput.incomeTax;
-    let priceData = userInput.priceData;
-    let initialInvestment = userInput.initialInvestment;
-
+export async function gatherData(start, end, network, address, currency, incomeTax, priceData, initialInvestment){
+   
     let obj = {};
     let daysArray = [];
 
