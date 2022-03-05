1acf2eeecfc2078152f7eb2c7e5c943b43edbe66
Jesse Mykolyn <jmykolyn@wealthsimple.com>
2022-03-04 22:26:23 -0500

    chore: Format

--- src/index.js
+++ src/index.js
@@ -1,118 +1,179 @@
-import { gatherData } from './gatherData.js';
-import { exportVariable, readJSON, writeCSV, writeOverviewCSV } from './fileWorker.js';
-import { calculateMetrics, verifyUserInput, getTicker, checkPriceAvailablilty } from './utils.js';
+import { gatherData } from "./gatherData.js";
+import {
+  exportVariable,
+  readJSON,
+  writeCSV,
+  writeOverviewCSV,
+} from "./fileWorker.js";
+import {
+  calculateMetrics,
+  verifyUserInput,
+  getTicker,
+  checkPriceAvailablilty,
+} from "./utils.js";
 
-
-async function main () {
+async function main() {
   let obj = {};
   let csv = [];
-  let userInput = readJSON('config/userInput.json');
+  let userInput = readJSON("config/userInput.json");
 
   let numberPayouts = {
-    "DOT": 0,
-    "KSM": 0,
-    "MOVR":0,
-    "GLMR": 0,
-    "SDN": 0,
-    "ASTR": 0,
-  }
+    DOT: 0,
+    KSM: 0,
+    MOVR: 0,
+    GLMR: 0,
+    SDN: 0,
+    ASTR: 0,
+  };
   let totalStaked = {
-    "DOT": 0,
-    "KSM": 0,
-    "MOVR": 0,
-    "GLMR": 0,
-    "SDN": 0,
-    "ASTR": 0,
-  }
+    DOT: 0,
+    KSM: 0,
+    MOVR: 0,
+    GLMR: 0,
+    SDN: 0,
+    ASTR: 0,
+  };
 
   let totalFiatToken = {
-    "DOT": 0,
-    "KSM": 0,
-    "MOVR": 0,
-    "GLMR": 0,
-    "SDN": 0,
-    "ASTR": 0,
-  }
+    DOT: 0,
+    KSM: 0,
+    MOVR: 0,
+    GLMR: 0,
+    SDN: 0,
+    ASTR: 0,
+  };
 
   let totalFiat = 0;
 
-
-  for(let i = 0; i < userInput.addresses.length; i++){
+  for (let i = 0; i < userInput.addresses.length; i++) {
     verifyUserInput(userInput);
     let network = userInput.addresses[i].network.toLowerCase();
     let priceData = checkPriceAvailablilty(userInput, network);
     let start = userInput.start;
     let end = userInput.end;
-    let address = userInput.addresses[i].address;   
+    let address = userInput.addresses[i].address;
     let currency = userInput.currency;
     let exportOutput = userInput.exportOutput;
     let startBalance = userInput.addresses[i].startBalance;
     let ticker = getTicker(network);
 
-    obj = await gatherData(start, end, network, address, currency, priceData, startBalance, ticker);
-    
+    obj = await gatherData(
+      start,
+      end,
+      network,
+      address,
+      currency,
+      priceData,
+      startBalance,
+      ticker
+    );
+
     // otherwise there were no rewards
-    if(obj.data.numberRewardsParsed > 0){
+    if (obj.data.numberRewardsParsed > 0) {
       obj = calculateMetrics(obj);
     }
 
-    if(exportOutput == "true" & obj.message != 'No rewards found for this address'){ 
-      exportVariable(JSON.stringify(obj), userInput.addresses[i].name + ' ' + obj.address + '.json'); 
-      writeCSV(obj, userInput.addresses[i].name + ' ' + obj.address + '.csv');
+    if (
+      (exportOutput == "true") &
+      (obj.message != "No rewards found for this address")
+    ) {
+      exportVariable(
+        JSON.stringify(obj),
+        userInput.addresses[i].name + " " + obj.address + ".json"
+      );
+      writeCSV(obj, userInput.addresses[i].name + " " + obj.address + ".csv");
     }
     /* 
     Creates an overview csv that holds a summary of all addresses. I need to pass it outside of the previous if-condition because it could be that the last address didn't have any rewards which
     would lead to the fact that the file would never be written. I included a flag in the writeOverviewCSV function to skip writing a line for addresses that have no rewards.
     */
-    if(exportOutput == "true"){
-    csv = writeOverviewCSV(i, userInput.addresses.length, obj, csv);
+    if (exportOutput == "true") {
+      csv = writeOverviewCSV(i, userInput.addresses.length, obj, csv);
     }
-
 
     totalFiat = totalFiat + obj.totalValueFiat;
 
-    if(network == "polkadot"){
-        totalStaked.DOT = totalStaked.DOT + obj.totalAmountHumanReadable;
-        numberPayouts.DOT = numberPayouts.DOT + obj.data.numberRewardsParsed;
-        totalFiatToken.DOT = totalFiatToken.DOT + obj.totalValueFiat;
+    if (network == "polkadot") {
+      totalStaked.DOT = totalStaked.DOT + obj.totalAmountHumanReadable;
+      numberPayouts.DOT = numberPayouts.DOT + obj.data.numberRewardsParsed;
+      totalFiatToken.DOT = totalFiatToken.DOT + obj.totalValueFiat;
     } else if (network == "kusama") {
-        numberPayouts.KSM = numberPayouts.KSM + obj.data.numberRewardsParsed;
-        totalStaked.KSM = totalStaked.KSM + obj.totalAmountHumanReadable;
-        totalFiatToken.KSM = totalFiatToken.KSM + obj.totalValueFiat;
-    } else if (network == "moonriver"){
-        numberPayouts.MOVR = numberPayouts.MOVR + obj.data.numberRewardsParsed;
-        totalStaked.MOVR = totalStaked.MOVR + obj.totalAmountHumanReadable;
-        totalFiatToken.MOVR = totalFiatToken.MOVR + obj.totalValueFiat;
-    } else if (network == "moonbeam"){
-        numberPayouts.GLMR = numberPayouts.GLMR + obj.data.numberRewardsParsed
-        totalStaked.GLMR = totalStaked.GLMR + obj.totalAmountHumanReadable;
-        totalFiatToken.GLMR = totalFiatToken.GLMR + obj.totalValueFiat;
-    } else if (network == "shiden"){
-        numberPayouts.SDN = numberPayouts.SDN + obj.data.numberRewardsParsed
-        totalStaked.SDN = totalStaked.SDN + obj.totalAmountHumanReadable;
-        totalFiatToken.SDN = totalFiatToken.SDN + obj.totalValueFiat;
-    } else if (network == "astar"){
-      numberPayouts.ASTR = numberPayouts.ASTR + obj.data.numberRewardsParsed
+      numberPayouts.KSM = numberPayouts.KSM + obj.data.numberRewardsParsed;
+      totalStaked.KSM = totalStaked.KSM + obj.totalAmountHumanReadable;
+      totalFiatToken.KSM = totalFiatToken.KSM + obj.totalValueFiat;
+    } else if (network == "moonriver") {
+      numberPayouts.MOVR = numberPayouts.MOVR + obj.data.numberRewardsParsed;
+      totalStaked.MOVR = totalStaked.MOVR + obj.totalAmountHumanReadable;
+      totalFiatToken.MOVR = totalFiatToken.MOVR + obj.totalValueFiat;
+    } else if (network == "moonbeam") {
+      numberPayouts.GLMR = numberPayouts.GLMR + obj.data.numberRewardsParsed;
+      totalStaked.GLMR = totalStaked.GLMR + obj.totalAmountHumanReadable;
+      totalFiatToken.GLMR = totalFiatToken.GLMR + obj.totalValueFiat;
+    } else if (network == "shiden") {
+      numberPayouts.SDN = numberPayouts.SDN + obj.data.numberRewardsParsed;
+      totalStaked.SDN = totalStaked.SDN + obj.totalAmountHumanReadable;
+      totalFiatToken.SDN = totalFiatToken.SDN + obj.totalValueFiat;
+    } else if (network == "astar") {
+      numberPayouts.ASTR = numberPayouts.ASTR + obj.data.numberRewardsParsed;
       totalStaked.ASTR = totalStaked.ASTR + obj.totalAmountHumanReadable;
       totalFiatToken.ASTR = totalFiatToken.ASTR + obj.totalValueFiat;
     }
   }
 
-    
-  console.log('The following table lists all found rewards and values are expressed in ' + obj.currency);
+  console.log(
+    "The following table lists all found rewards and values are expressed in " +
+      obj.currency
+  );
 
+  const DOT = {
+    Name: "DOT",
+    "Nr. Payouts": numberPayouts.DOT,
+    "Number of Tokens": totalStaked.DOT,
+    Value: totalFiatToken.DOT,
+  };
+  const KSM = {
+    Name: "KSM",
+    "Nr. Payouts": numberPayouts.KSM,
+    "Number of Tokens": totalStaked.KSM,
+    Value: totalFiatToken.KSM,
+  };
+  const GLMR = {
+    Name: "GLMR",
+    "Nr. Payouts": numberPayouts.GLMR,
+    "Number of Tokens": totalStaked.GLMR,
+    Value: totalFiatToken.GLMR,
+  };
+  const MOVR = {
+    Name: "MOVR",
+    "Nr. Payouts": numberPayouts.MOVR,
+    "Number of Tokens": totalStaked.MOVR,
+    Value: totalFiatToken.MOVR,
+  };
+  const SDN = {
+    Name: "SDN",
+    "Nr. Payouts": numberPayouts.SDN,
+    "Number of Tokens": totalStaked.SDN,
+    Value: totalFiatToken.SDN,
+  };
+  const ASTR = {
+    Name: "ASTR",
+    "Nr. Payouts": numberPayouts.ASTR,
+    "Number of Tokens": totalStaked.ASTR,
+    Value: totalFiatToken.ASTR,
+  };
 
-  const DOT = {"Name": "DOT", "Nr. Payouts": numberPayouts.DOT, "Number of Tokens": totalStaked.DOT, "Value": totalFiatToken.DOT};
-  const KSM = {"Name": "KSM", "Nr. Payouts": numberPayouts.KSM, "Number of Tokens": totalStaked.KSM, "Value": totalFiatToken.KSM};
-  const GLMR = {"Name": "GLMR", "Nr. Payouts": numberPayouts.GLMR, "Number of Tokens": totalStaked.GLMR, "Value": totalFiatToken.GLMR}
-  const MOVR = {"Name": "MOVR", "Nr. Payouts": numberPayouts.MOVR, "Number of Tokens": totalStaked.MOVR, "Value": totalFiatToken.MOVR};
-  const SDN = {"Name": "SDN", "Nr. Payouts": numberPayouts.SDN, "Number of Tokens": totalStaked.SDN, "Value": totalFiatToken.SDN};
-  const ASTR = {"Name": "ASTR", "Nr. Payouts": numberPayouts.ASTR, "Number of Tokens": totalStaked.ASTR, "Value": totalFiatToken.ASTR};
-
-
-  
   console.table([DOT, KSM, GLMR, MOVR, ASTR, SDN]);
-  console.log('The total value of all payouts is ' + totalFiat + ' ' + obj.currency + ' (based on daily prices).');
-  console.log('For more information, open the CSV file(s) or copy the content of the JSON file(s) into http://jsonviewer.stack.hu/ (click format).'); 
+  console.log(
+    "The total value of all payouts is " +
+      totalFiat +
+      " " +
+      obj.currency +
+      " (based on daily prices)."
+  );
+  console.log(
+    "For more information, open the CSV file(s) or copy the content of the JSON file(s) into http://jsonviewer.stack.hu/ (click format)."
+  );
 }
-main().catch(console.error).finally(() => process.exit());
+main()
+  .catch(console.error)
+  .finally(() => process.exit());
--- src/utils.js
+++ src/utils.js
@@ -1,278 +1,344 @@
-import { round, pow } from 'mathjs';
+import { round, pow } from "mathjs";
 
 export function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function dateToString(date) {
+  let day = date.getDate().toString();
+  let month = (date.getMonth() + 1).toString();
+  let year = date.getFullYear().toString();
+
+  if (day.length == 1) {
+    day = day.concat("0");
+    day = _reverseString(day);
   }
 
-export function dateToString(date){
-    let day = date.getDate().toString();
-    let month = (date.getMonth() + 1).toString();
-    let year = date.getFullYear().toString();
-  
-    if(day.length == 1){
-      day = day.concat('0');
-      day = _reverseString(day);
-    }
-  
-    if(month.length == 1){
-      month = month.concat('0');
-      month = _reverseString(month);
-    }
-    return day.concat('-', month, '-', year);
+  if (month.length == 1) {
+    month = month.concat("0");
+    month = _reverseString(month);
   }
+  return day.concat("-", month, "-", year);
+}
 
 export function makeDaysArray(startDate, endDate) {
-    let dates = [];
-    const theDate = new Date(startDate);
-    while (theDate < endDate) {
-      dates = [...dates, new Date(theDate)];
-      theDate.setDate(theDate.getDate() + 1);
-    }
-    dates = [...dates, endDate];
-    dates = _transformArrayToString(dates);
-    return dates;
+  let dates = [];
+  const theDate = new Date(startDate);
+  while (theDate < endDate) {
+    dates = [...dates, new Date(theDate)];
+    theDate.setDate(theDate.getDate() + 1);
+  }
+  dates = [...dates, endDate];
+  dates = _transformArrayToString(dates);
+  return dates;
+}
+
+export function initializeObject(
+  daysArray,
+  network,
+  address,
+  currency,
+  startBalance,
+  ticker
+) {
+  let obj = {
+    message: "empty",
+    address: address,
+    network: network,
+    ticker: ticker,
+    currency: currency,
+    startBalance: startBalance,
+    firstReward: "",
+    lastReward: "",
+    annualizedReturn: 0,
+    currentValueRewardsFiat: 0,
+    totalAmountHumanReadable: 0,
+    totalValueFiat: 0,
+    data: {
+      numberRewardsParsed: 0,
+      numberOfDays: daysArray.length,
+      list: [],
+    },
   };
-
-export function initializeObject(daysArray, network, address, currency, startBalance, ticker){
-    let obj = {
-        'message': 'empty',
-        'address': address,
-        'network': network,
-        'ticker' : ticker,
-        'currency': currency,
-        'startBalance': startBalance,
-        'firstReward': '',
-        'lastReward': '',
-        'annualizedReturn':0,
-        'currentValueRewardsFiat':0,
-        'totalAmountHumanReadable':0,
-        'totalValueFiat': 0,
-        'data':{
-            'numberRewardsParsed': 0,
-            'numberOfDays': daysArray.length,
-            'list':[]
-        }
-    }
-    for(let i = 0; i < daysArray.length; i++){
-        obj.data.list[i] = {
-            'day' : daysArray[i],
-            'blockNumber': '',
-            'extrinsicHash': '',
-            'price': 0,
-            'volume': 0,
-            'amountPlanks': 0,
-            'numberPayouts':0,
-            'amountHumanReadable': 0,
-            'valueFiat':0
-        }
-    }
-    return obj;
+  for (let i = 0; i < daysArray.length; i++) {
+    obj.data.list[i] = {
+      day: daysArray[i],
+      blockNumber: "",
+      extrinsicHash: "",
+      price: 0,
+      volume: 0,
+      amountPlanks: 0,
+      numberPayouts: 0,
+      amountHumanReadable: 0,
+      valueFiat: 0,
+    };
+  }
+  return obj;
 }
 
-function _reverseString(string){
-    var i;
-    let length = string.length;
-    var tmp_string = '';
-  
-    for(i = 0; i < string.length; i++){
-      tmp_string = tmp_string.concat(string[length-1]);
-      length -= 1;
-    }
-    return tmp_string;
+function _reverseString(string) {
+  var i;
+  let length = string.length;
+  var tmp_string = "";
+
+  for (i = 0; i < string.length; i++) {
+    tmp_string = tmp_string.concat(string[length - 1]);
+    length -= 1;
+  }
+  return tmp_string;
 }
 
-function _transformArrayToString(array){
-    let newArray = [];
-  
-    for(let i = 0; i < array.length; i++){
-      newArray[i] = dateToString(array[i]);
-    }
-    return newArray;
+function _transformArrayToString(array) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = dateToString(array[i]);
+  }
+  return newArray;
 }
 
-export function transformDDMMYYYtoUnix(dateString){
-
-    var dateParts = dateString.split('-');
-    let date = new Date(Date.UTC(+dateParts[2], dateParts[1] - 1, +dateParts[0]));
-    // CoinGecko defines a day at 12:00am (UTC) which is 7200 after the value given by the date.valueOf()
-    let unix = date.valueOf() / 1000;
-    return unix;
+export function transformDDMMYYYtoUnix(dateString) {
+  var dateParts = dateString.split("-");
+  let date = new Date(Date.UTC(+dateParts[2], dateParts[1] - 1, +dateParts[0]));
+  // CoinGecko defines a day at 12:00am (UTC) which is 7200 after the value given by the date.valueOf()
+  let unix = date.valueOf() / 1000;
+  return unix;
 }
 
-export function min(a,b){
-    var min;
-    if(a>b){
-        min = b; 
-    }else {
-        min = a;
-    }
-    return min;
+export function min(a, b) {
+  var min;
+  if (a > b) {
+    min = b;
+  } else {
+    min = a;
+  }
+  return min;
 }
 
-export function calculateMetrics(obj){
-    var normalization;
+export function calculateMetrics(obj) {
+  var normalization;
 
-    normalization = _getDenomination(obj.network);
+  normalization = _getDenomination(obj.network);
 
-    for(let i = 0; i < obj.data.numberOfDays; i++){
-        // generate new metrics
-        obj.data.list[i].amountHumanReadable = obj.data.list[i].amountPlanks * normalization;
-        obj.data.list[i].valueFiat = obj.data.list[i].amountHumanReadable * obj.data.list[i].price;
-        obj.data.list[i].price = obj.data.list[i].price;
+  for (let i = 0; i < obj.data.numberOfDays; i++) {
+    // generate new metrics
+    obj.data.list[i].amountHumanReadable =
+      obj.data.list[i].amountPlanks * normalization;
+    obj.data.list[i].valueFiat =
+      obj.data.list[i].amountHumanReadable * obj.data.list[i].price;
+    obj.data.list[i].price = obj.data.list[i].price;
 
-        // add values of each day to general metrics.
-        obj.totalValueFiat = obj.totalValueFiat + obj.data.list[i].valueFiat;
-        obj.totalAmountHumanReadable = obj.totalAmountHumanReadable + obj.data.list[i].amountHumanReadable;
-    }
+    // add values of each day to general metrics.
+    obj.totalValueFiat = obj.totalValueFiat + obj.data.list[i].valueFiat;
+    obj.totalAmountHumanReadable =
+      obj.totalAmountHumanReadable + obj.data.list[i].amountHumanReadable;
+  }
 
-    obj.totalValueFiat = round(obj.totalValueFiat,2);
-    obj.currentValueRewardsFiat = round(obj.totalAmountHumanReadable * obj.data.list[0].price,2);
-    obj.annualizedReturn = _calculateAnnualizedReturn(obj);
-    
-    return obj;
+  obj.totalValueFiat = round(obj.totalValueFiat, 2);
+  obj.currentValueRewardsFiat = round(
+    obj.totalAmountHumanReadable * obj.data.list[0].price,
+    2
+  );
+  obj.annualizedReturn = _calculateAnnualizedReturn(obj);
+
+  return obj;
 }
 
-function _calculateAnnualizedReturn(obj){
-    var annualized;
-    var firstAndLastReward;
-    var daysBetweenRewards;
+function _calculateAnnualizedReturn(obj) {
+  var annualized;
+  var firstAndLastReward;
+  var daysBetweenRewards;
 
-    firstAndLastReward = _getFirstandLastReward(obj);
-    obj.firstReward = firstAndLastReward.firstReward;
-    obj.lastReward = firstAndLastReward.lastReward;
-    //added one day because users must lock for one day and wait.
-    daysBetweenRewards =  ((transformDDMMYYYtoUnix(obj.lastReward) - transformDDMMYYYtoUnix(obj.firstReward)) / 60 / 60 / 24) + 1;
-    let rateOfReturn = 1 + obj.totalAmountHumanReadable /obj.startBalance;
-    let daysFraction = 365 / daysBetweenRewards;
-    annualized = pow(rateOfReturn,daysFraction) - 1;
+  firstAndLastReward = _getFirstandLastReward(obj);
+  obj.firstReward = firstAndLastReward.firstReward;
+  obj.lastReward = firstAndLastReward.lastReward;
+  //added one day because users must lock for one day and wait.
+  daysBetweenRewards =
+    (transformDDMMYYYtoUnix(obj.lastReward) -
+      transformDDMMYYYtoUnix(obj.firstReward)) /
+      60 /
+      60 /
+      24 +
+    1;
+  let rateOfReturn = 1 + obj.totalAmountHumanReadable / obj.startBalance;
+  let daysFraction = 365 / daysBetweenRewards;
+  annualized = pow(rateOfReturn, daysFraction) - 1;
 
-    return annualized;
+  return annualized;
 }
 
-function _getFirstandLastReward(obj){
-    let i = 0;
-    let max = obj.data.numberOfDays;
-    let x = max - 1;
-    var firstReward;
-    var lastReward;
+function _getFirstandLastReward(obj) {
+  let i = 0;
+  let max = obj.data.numberOfDays;
+  let x = max - 1;
+  var firstReward;
+  var lastReward;
 
-    while (i < max) {
-        if (obj.data.list[i].numberPayouts != 0) {
-            firstReward = obj.data.list[i].day; 
-            break;
-        } 
-        i++;
+  while (i < max) {
+    if (obj.data.list[i].numberPayouts != 0) {
+      firstReward = obj.data.list[i].day;
+      break;
     }
+    i++;
+  }
 
-    while (x >= 0) {
-        if (obj.data.list[x].numberPayouts != 0) {    
-           lastReward = obj.data.list[x].day;
-            break;
-        }
-        x--;
+  while (x >= 0) {
+    if (obj.data.list[x].numberPayouts != 0) {
+      lastReward = obj.data.list[x].day;
+      break;
     }
-    return {
-        'firstReward': firstReward,
-        'lastReward': lastReward
-    }
+    x--;
+  }
+  return {
+    firstReward: firstReward,
+    lastReward: lastReward,
+  };
 }
 
-export function verifyUserInput(userInput){
-    let start = new Date(userInput.start);
-    let end = new Date(userInput.end);
+export function verifyUserInput(userInput) {
+  let start = new Date(userInput.start);
+  let end = new Date(userInput.end);
 
-    if(start > end){
-        throw new Error('Start date must be before end date')
-    }
+  if (start > end) {
+    throw new Error("Start date must be before end date");
+  }
 
-    if(end > new Date()){
-        throw new Error('End date is in the future.');
-    }
+  if (end > new Date()) {
+    throw new Error("End date is in the future.");
+  }
 }
 
-export function getTicker(network){
-    var ticker;
-    // Convert to upper case to avoid issue with different user input.
-    switch(network){
-        case 'kusama':
-             ticker = 'KSM';
-             break;
-        case 'polkadot':
-            ticker = 'DOT';
-            break;
-        case 'moonriver':
-            ticker = 'MOVR';
-            break;     
-        case 'moonbeam':
-            ticker = 'GLMR';
-            break;
-        case 'shiden':
-            ticker = 'SDN';
-            break;
-        case 'astar':
-            ticker = 'ASTR';
-            break;
-    }
-    return ticker;
+export function getTicker(network) {
+  var ticker;
+  // Convert to upper case to avoid issue with different user input.
+  switch (network) {
+    case "kusama":
+      ticker = "KSM";
+      break;
+    case "polkadot":
+      ticker = "DOT";
+      break;
+    case "moonriver":
+      ticker = "MOVR";
+      break;
+    case "moonbeam":
+      ticker = "GLMR";
+      break;
+    case "shiden":
+      ticker = "SDN";
+      break;
+    case "astar":
+      ticker = "ASTR";
+      break;
+  }
+  return ticker;
 }
 
-export function _getDenomination(network){
-    var normalization;
-    switch(network){
-        case 'polkadot':
-            normalization = 1/10000000000;
-            break;
-        case 'kusama': 
-            normalization = 1/1000000000000;
-            break;
-        case 'moonriver': 
-            normalization = 1/1000000000000000000;
-            break;
-        case 'moonbeam':
-            normalization = 1/1000000000000000000;
-            break;
-        case 'shiden':
-            normalization = 1/1000000000000000000;
-            break;
-        case 'astar':
-            normalization = 1/1000000000000000000;
-            break;
-    }
-    return normalization;
+export function _getDenomination(network) {
+  var normalization;
+  switch (network) {
+    case "polkadot":
+      normalization = 1 / 10000000000;
+      break;
+    case "kusama":
+      normalization = 1 / 1000000000000;
+      break;
+    case "moonriver":
+      normalization = 1 / 1000000000000000000;
+      break;
+    case "moonbeam":
+      normalization = 1 / 1000000000000000000;
+      break;
+    case "shiden":
+      normalization = 1 / 1000000000000000000;
+      break;
+    case "astar":
+      normalization = 1 / 1000000000000000000;
+      break;
+  }
+  return normalization;
 }
 
-export function checkPriceAvailablilty(userInput, network){
-    let priceData = userInput.priceData;
-    let end = new Date(userInput.end);
+export function checkPriceAvailablilty(userInput, network) {
+  let priceData = userInput.priceData;
+  let end = new Date(userInput.end);
 
-    if(end.valueOf() < 1597708800000 & network == 'polkadot' & priceData == 'true'){
-        console.log('Your requested time window lies before prices are available for ' + network.toUpperCase() + '. Switching off price data.');
-        priceData = 'false';
-    }
-    
-    if(end.valueOf() < 1568851200000 & network == 'kusama' & priceData == 'true'){
-        console.log('Your requested time window lies before prices are available for ' + network.toUpperCase() + '. Switching off price data.');
-        priceData = 'false';
-    }
+  if (
+    (end.valueOf() < 1597708800000) &
+    (network == "polkadot") &
+    (priceData == "true")
+  ) {
+    console.log(
+      "Your requested time window lies before prices are available for " +
+        network.toUpperCase() +
+        ". Switching off price data."
+    );
+    priceData = "false";
+  }
 
-    if(end.valueOf() < 1630022400000 & network == 'moonriver' & priceData == 'true'){
-        console.log('Your requested time window lies before prices are available for ' + network.toUpperCase() + '. Switching off price data.');
-        priceData = 'false';
-    }
+  if (
+    (end.valueOf() < 1568851200000) &
+    (network == "kusama") &
+    (priceData == "true")
+  ) {
+    console.log(
+      "Your requested time window lies before prices are available for " +
+        network.toUpperCase() +
+        ". Switching off price data."
+    );
+    priceData = "false";
+  }
 
-    if(end.valueOf() < 1641884400000 & network == 'moonbeam' & priceData == 'true'){
-        console.log('Your requested time window lies before prices are available for ' + network.toUpperCase() + '. Switching off price data.');
-        priceData = 'false';
-    }
+  if (
+    (end.valueOf() < 1630022400000) &
+    (network == "moonriver") &
+    (priceData == "true")
+  ) {
+    console.log(
+      "Your requested time window lies before prices are available for " +
+        network.toUpperCase() +
+        ". Switching off price data."
+    );
+    priceData = "false";
+  }
 
-    if(end.valueOf() < 1630303200000 & network == 'shiden' & priceData == 'true'){
-        console.log('Your requested time window lies before prices are available for ' + network.toUpperCase() + '. Switching off price data.');
-        priceData = 'false';
-    }
+  if (
+    (end.valueOf() < 1641884400000) &
+    (network == "moonbeam") &
+    (priceData == "true")
+  ) {
+    console.log(
+      "Your requested time window lies before prices are available for " +
+        network.toUpperCase() +
+        ". Switching off price data."
+    );
+    priceData = "false";
+  }
 
-    if(end.valueOf() < 1642402800000 & network == 'astar' & priceData == 'true'){
-        console.log('Your requested time window lies before prices are available for ' + network.toUpperCase() + '. Switching off price data.');
-        priceData = 'false';
-    } 
-    return priceData;
+  if (
+    (end.valueOf() < 1630303200000) &
+    (network == "shiden") &
+    (priceData == "true")
+  ) {
+    console.log(
+      "Your requested time window lies before prices are available for " +
+        network.toUpperCase() +
+        ". Switching off price data."
+    );
+    priceData = "false";
+  }
+
+  if (
+    (end.valueOf() < 1642402800000) &
+    (network == "astar") &
+    (priceData == "true")
+  ) {
+    console.log(
+      "Your requested time window lies before prices are available for " +
+        network.toUpperCase() +
+        ". Switching off price data."
+    );
+    priceData = "false";
+  }
+  return priceData;
 }
