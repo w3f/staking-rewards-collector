33ea209e1d45f012c86964893205233fe0161767
Jesse Mykolyn <jmykolyn@wealthsimple.com>
2022-03-11 18:24:42 -0500

    Add tests for makeDaysArray() util

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,4 +1,4 @@
-import { sleep, dateToString } from './utils';
+import { sleep, dateToString, makeDaysArray } from './utils';
 
 describe('sleep()', () => {
   it('should return a Promise that resolves after the number of milliseconds provided', async () => {
@@ -28,5 +28,25 @@ describe('dateToString()', () => {
     const result = dateToString(date);
 
     expect(result).toBe('07-03-2022');
+  });
+});
+
+describe('makeDaysArray()', () => {
+  it('should return an inclusive range of date strings using the "start" and "end" dates provided', () => {
+    const start = new Date('Mon Mar 7 2022');
+    const end = new Date('Mon Mar 14 2022');
+
+    const result = makeDaysArray(start, end);
+
+    expect(result).toEqual([
+      '07-03-2022',
+      '08-03-2022',
+      '09-03-2022',
+      '10-03-2022',
+      '11-03-2022',
+      '12-03-2022',
+      '13-03-2022',
+      '14-03-2022',
+    ]);
   });
 });
