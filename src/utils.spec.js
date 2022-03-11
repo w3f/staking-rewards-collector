8e4b7fa6ef0ee97d6873fcacfa85b27a9886d343
Jesse Mykolyn <jmykolyn@wealthsimple.com>
2022-03-11 18:30:37 -0500

    Add tests for initializeObject() util

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,4 +1,4 @@
-import { sleep, dateToString, makeDaysArray } from './utils';
+import { sleep, dateToString, makeDaysArray, initializeObject } from './utils';
 
 describe('sleep()', () => {
   it('should return a Promise that resolves after the number of milliseconds provided', async () => {
@@ -48,5 +48,69 @@ describe('makeDaysArray()', () => {
       '13-03-2022',
       '14-03-2022',
     ]);
+  });
+});
+
+describe('initializeObject()', () => {
+  it('should enhance the data provided and return the result', () => {
+    const days = ['07-03-2022', '07-04-2022'];
+    const network = 'foo';
+    const address = 'bar';
+    const currency = 'baz';
+    const startBalance = 0;
+    const ticker = 'quux';
+    const expected = {
+      'address': 'bar',
+      'annualizedReturn': 0,
+      'currency': 'baz',
+      'currentValueRewardsFiat': 0,
+      'data': {
+        'list': [
+          {
+            'amountHumanReadable': 0,
+            'amountPlanks': 0,
+            'blockNumber': '',
+            'day': '07-03-2022',
+            'extrinsicHash': '',
+            'numberPayouts': 0,
+            'price': 0,
+            'valueFiat': 0,
+            'volume': 0,
+          },
+          {
+            'amountHumanReadable': 0,
+            'amountPlanks': 0,
+            'blockNumber': '',
+            'day': '07-04-2022',
+            'extrinsicHash': '',
+            'numberPayouts': 0,
+            'price': 0,
+            'valueFiat': 0,
+            'volume': 0,
+          },
+        ],
+        'numberOfDays': 2,
+        'numberRewardsParsed': 0,
+      },
+      'firstReward': '',
+      'lastReward': '',
+      'message': 'empty',
+      'network': 'foo',
+      'startBalance': 0,
+      'ticker': 'quux',
+      'totalAmountHumanReadable': 0,
+      'totalValueFiat': 0,
+    };
+
+    const result = initializeObject(
+      days,
+      network,
+      address,
+      currency,
+      startBalance,
+      ticker,
+    );
+
+    expect(result).toEqual(expected);
   });
 });
