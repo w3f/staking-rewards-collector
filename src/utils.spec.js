16ac280e91c2e39740723645df7e3511fe3e9f58
Jesse Mykolyn <jmykolyn@wealthsimple.com>
2022-03-11 18:19:47 -0500

    Add tests for dateToString() util

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,4 +1,4 @@
-import { sleep } from './utils';
+import { sleep, dateToString } from './utils';
 
 describe('sleep()', () => {
   it('should return a Promise that resolves after the number of milliseconds provided', async () => {
@@ -10,5 +10,23 @@ describe('sleep()', () => {
     const end = new Date().getTime();
 
     expect(end).toBeGreaterThanOrEqual(start + duration);
+  });
+});
+
+describe('dateToString()', () => {
+  it('should convert a Date instance into the corresponding DD-MM-YYYY string', () => {
+    const date = new Date('Mon Dec 12 2022');
+
+    const result = dateToString(date);
+
+    expect(result).toBe('12-12-2022');
+  });
+
+  it('should pad single digit month and day values', () => {
+    const date = new Date('Mon Mar 7 2022');
+
+    const result = dateToString(date);
+
+    expect(result).toBe('07-03-2022');
   });
 });
