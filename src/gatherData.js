19ee1189384f71aa6b8e2457ba9ca5431f069d22
Alex Chien <alexchien97@gmail.com>
2022-05-06 16:11:39 +0800

    add `subscan_apikey` config item and use that apikey when calling subscan api service

--- src/gatherData.js
+++ src/gatherData.js
@@ -3,14 +3,14 @@ import { makeDaysArray, initializeObject } from "./utils.js";
 import { addStakingData } from './curl.js';
 
 export async function gatherData(
-    start, end, network, name, address, currency, priceData, startBalance, ticker
+    start, end, network, name, address, currency, priceData, startBalance, ticker, subscan_apikey
 ) {
    
     let obj = {};
     let daysArray = [];
 
     daysArray = makeDaysArray(new Date(start), new Date(end));
-    obj = initializeObject(daysArray, network, name, address, currency, startBalance, ticker);
+    obj = initializeObject(daysArray, network, name, address, currency, startBalance, ticker, subscan_apikey);
     if(priceData == 'true'){
         obj = await addPriceData(obj);
     }
