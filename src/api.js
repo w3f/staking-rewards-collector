628e26b2a54ded9ad8544f383bac91e0f1001ad3
Pieky <pieky@proton.me>
2024-10-12 13:40:54 +0200

    Update api.js
    add capabilities for custom coingecki API key

--- src/api.js
+++ src/api.js
@@ -3,6 +3,20 @@ import { round } from 'mathjs';
 import { getCoinGeckoName, getNetworkTimeMinimum } from './networks.js';
 import { sleep, transformDDMMYYYtoUnix } from './utils.js';
 
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url'; 
+import { dirname } from 'path';
+  
+// Resolve directory name 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename); 
+  
+// Load configuration  
+const configPath = path.resolve(__dirname, '../config/userInput.json'); 
+const config = JSON.parse(await fs.readFile(configPath, 'utf8')); 
+const coingeckoApiKey = config.coingecko_apikey;
+
 export async function addPriceData(obj){
     let priceObject = await _getPriceObject(obj);
     var prices;
@@ -25,7 +39,12 @@ export async function addPriceData(obj){
 }
 
 async function _getPriceObject(obj){
-    const CoinGeckoClient = new CoinGecko();
+       const CoinGeckoClient = new CoinGecko({  
+        headers: {  
+            'x_cg_pro_api_key': coingeckoApiKey  
+        }  
+    }); 
+    
     var priceObject;
     let start = transformDDMMYYYtoUnix(obj.data.list[0].day);
     let end = transformDDMMYYYtoUnix(obj.data.list.slice(-1)[0].day);
